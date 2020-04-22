85da16d42e096374903ff5337b5f5da6925928ea
Mercedes Dorsey <dorseymercedes@gmail.com>
2020-04-22 19:28:02 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,7 +16,7 @@ def shipping_manifest
 end
 
 def retrieval
-  shipping_manifest = "whale bone corsets" => 5, 
+  shipping_manifest = {"whale bone corsets" => 5, 
     "porcelain vases" => 2, 
     "oil paintings" => 3
     }
@@ -24,3 +24,4 @@ def retrieval
     shipping_manifest["oil paintings"]
 
 end
+
